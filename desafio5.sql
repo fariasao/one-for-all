53f53f26ffd20cfbfef603bdfb89c20df74d0e30
fariasao <enzofarias2010@gmail.com>
2022-02-08 22:16:47 -0300

    identa requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,9 @@
-SELECT
-	m.musica AS cancao,
-    COUNT(r.musica_id) AS reproducoes
-FROM SpotifyClone.musicas m
-JOIN SpotifyClone.reproduzida r ON r.musica_id = m.musica_id
+SELECT 
+    m.musica AS cancao, COUNT(r.musica_id) AS reproducoes
+FROM
+    SpotifyClone.musicas m
+        JOIN
+    SpotifyClone.reproduzida r ON r.musica_id = m.musica_id
 GROUP BY m.musica
-ORDER BY reproducoes DESC, m.musica LIMIT 2;
+ORDER BY reproducoes DESC , m.musica
+LIMIT 2;
