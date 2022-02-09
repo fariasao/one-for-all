cf54e6872aad4adfbc321ab9a56ec54da321113e
fariasao <enzofarias2010@gmail.com>
2022-02-08 22:16:01 -0300

    requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT
+	m.musica AS cancao,
+    COUNT(r.musica_id) AS reproducoes
+FROM SpotifyClone.musicas m
+JOIN SpotifyClone.reproduzida r ON r.musica_id = m.musica_id
+GROUP BY m.musica
+ORDER BY reproducoes DESC, m.musica LIMIT 2;
