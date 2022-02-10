046252a8ed2e01ea29f8a75e9417eeb4eb5edbd9
fariasao <enzofarias2010@gmail.com>
2022-02-09 21:51:06 -0300

    requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+SELECT
+	ar.artista AS artista,
+    a.album AS album
+FROM SpotifyClone.albuns a
+
+INNER JOIN
+	SpotifyClone.artistas AS ar ON ar.artista = "Walter Phoenix" AND ar.artista_id = a.artista_id
+GROUP BY
+	a.album,
+    ar.artista
+ORDER BY
+	a.album;
