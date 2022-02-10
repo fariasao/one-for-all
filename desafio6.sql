9997670f652402d97e63860beefbbeafd36bdbbd
fariasao <enzofarias2010@gmail.com>
2022-02-09 21:45:19 -0300

    fix ident

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,9 @@
-SELECT
-	MIN(p.plano_preco) AS faturamento_minimo,
+SELECT 
+    MIN(p.plano_preco) AS faturamento_minimo,
     MAX(p.plano_preco) AS faturamento_maximo,
-    ROUND(AVG(p.plano_preco),2) AS faturamento_medio,
+    ROUND(AVG(p.plano_preco), 2) AS faturamento_medio,
     SUM(p.plano_preco) AS faturamento_total
-FROM SpotifyClone.planos p
-JOIN SpotifyClone.usuarios u
-ON p.plano_id = u.plano_id;
+FROM
+    SpotifyClone.planos p
+        JOIN
+    SpotifyClone.usuarios u ON p.plano_id = u.plano_id;
--- desafio7.sql
+++ desafio7.sql
@@ -1,17 +1,12 @@
-SELECT
-	ar.artista AS artista,
+SELECT 
+    ar.artista AS artista,
     a.album AS album,
     COUNT(s.artista_id) AS seguidores
 FROM
-	SpotifyClone.albuns a
-INNER JOIN
-	SpotifyClone.artistas AS ar ON ar.artista_id = a.artista_id
-INNER JOIN
-	SpotifyClone.seguindo AS s ON s.artista_id = ar.artista_id
-GROUP BY
-	a.album,
-    ar.artista
-ORDER BY
-	seguidores DESC,
-    artista,
-    album;
+    SpotifyClone.albuns a
+        INNER JOIN
+    SpotifyClone.artistas AS ar ON ar.artista_id = a.artista_id
+        INNER JOIN
+    SpotifyClone.seguindo AS s ON s.artista_id = ar.artista_id
+GROUP BY a.album , ar.artista
+ORDER BY seguidores DESC , artista , album;
